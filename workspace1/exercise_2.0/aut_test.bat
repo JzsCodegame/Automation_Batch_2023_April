4372dea9f7b90da0e2b0759ad19261c34b5893d0
Javid Zaman <zamanjavid@gmail.com>
2023-03-25 15:19:13 -0400

    3/25/23

--- workspace1/exercise_2.0/aut_test.bat
+++ workspace1/exercise_2.0/aut_test.bat
@@ -1,3 +1,3 @@
 @echo Off
 echo Cleaning project... &
-mvn clean & echo Compiling source code...& mvn compile & echo Compiling test source code... & mvn test-compile & echo Running tests... & mvn test
+mvn clean & echo Compiling source code...& mvn compile & echo Compiling test source code... & mvn test-compile & echo Running tests... & mvn install
